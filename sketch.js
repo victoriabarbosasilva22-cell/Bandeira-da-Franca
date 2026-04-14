e7cb6dc7af7ef6c2ae2891fb9093bc93ec86d647
Victória Barbosa Da Silva <victoria.barbosa.silva22@escola.pr.gov.br>
2026-04-14 08:54:42 -0300

    14-04-26

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  noStroke()
+        
+ // Azul
+ fill(0,85,164);
+ rect(0,0,200,400);
+  
+ // Branco
+ fill(255);
+ rect(200,0,200,400);
+  
+ // Vermelho
+  fill(239,65,53);
+  rect(400,0,200,400);
+}
